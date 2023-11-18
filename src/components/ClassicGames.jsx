bff3cc07429c646af4a591bb025b1253e783e882
syukrsl <134099202+syukrsl@users.noreply.github.com>
2023-11-18 11:26:53 +0800

    Add Classic Games Functionality

---
+++ src/components/ClassicGames.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function ClassicGames() {
+  // Classic games components and logic go here
+  return (
+    <div>
+      <h2>Classic Games</h2>
+      {/* Include game components */}
+    </div>
+  );
+}
+
+export default ClassicGames;
+  
