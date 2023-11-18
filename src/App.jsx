427e25af0b63411bb5522233455e96205ebf75bc
syukrsl <134099202+syukrsl@users.noreply.github.com>
2023-11-18 11:16:45 +0800

    Enhance WelcomePage & Add Styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,25 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom'; // Updated to Routes for v6
 import WelcomePage from './pages/WelcomePage.jsx';
 import SignIn from './pages/SignIn.jsx';
 import SignUp from './pages/SignUp.jsx';
 import TodoList from './components/TodoList.jsx';
 import MeetingNotes from './components/MeetingNotes.jsx';
 import ClassicGames from './components/ClassicGames.jsx';
-
+import News from './pages/News.jsx';
+import AuthenticatedHome from './pages/AuthenticatedHome.jsx';
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/sign-in" component={SignIn} />
-        <Route path="/sign-up" component={SignUp} />
-        <Route path="/todos" component={TodoList} />
-        <Route path="/notes" component={MeetingNotes} />
-        <Route path="/games" component={ClassicGames} />
-        <Route path="/" component={WelcomePage} />
-      </Switch>
-    </Router>
+    <Routes>
+      <Route path="/sign-in" element={<SignIn />} />
+      <Route path="/sign-up" element={<SignUp />} />
+      <Route path="/news" element={<News />} />
+      <Route path="/authenticated" element={<AuthenticatedHome />}></Route>
+      <Route path="/todos" element={<TodoList />} />
+      <Route path="/notes" element={<MeetingNotes />} />
+      <Route path="/games" element={<ClassicGames />} />
+      <Route path="/" element={<WelcomePage />} />
+    </Routes>
   );
 }
 
--- src/pages/WelcomePage.jsx
+++ src/pages/WelcomePage.jsx
@@ -1,17 +1,41 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./WelcomePage.css"; // Import your CSS file for styling
 
 function WelcomePage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = () => {
+    // Handle login logic here, e.g., making an API request
+  };
+
   return (
-    <div>
-      <h1>Welcome to Sphere</h1>
-      <nav>
-        <ul>
-          <li><Link to="/todos">To-Do List</Link></li>
-          <li><Link to="/notes">Meeting Notes</Link></li>
-          <li><Link to="/games">Classic Games</Link></li>
-        </ul>
-      </nav>
+    <div className="welcome-page">
+      <div className="login-card">
+        <h1>Welcome to CollabSphere</h1>
+
+        {/* Login form */}
+        <form>
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button onClick={handleLogin}>Login</button>
+        </form>
+
+        {/* Sign-up link */}
+        <p>Dont have an account? <Link to="/sign-up">Sign Up</Link></p>
+      </div>
     </div>
   );
 }
