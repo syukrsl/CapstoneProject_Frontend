896c7686908dc88507329ebe71721f322050162d
syukrsl <134099202+syukrsl@users.noreply.github.com>
2023-11-18 11:21:22 +0800

    Enhance SignIn Page & Add Styling

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,33 +1,43 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./SignIn.css"; // Import your CSS file for styling
 
 function SignIn() {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleSignIn = () => {
-    // Handle sign-in logic here (e.g., API request)
+    // Handle sign-in logic here, e.g., making an API request
   };
 
   return (
-    <div>
-      <h2>Sign In</h2>
-      <form>
-        <input
-          type="email"
-          placeholder="Email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          type="password"
-          placeholder="Password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <button onClick={handleSignIn}>Sign In</button>
-      </form>
-      <p>Dont have an account? <Link to="/sign-up">Sign Up</Link></p>
+    <div className="welcome-page">
+      <div className="login-card">
+        <h2>Sign In</h2>
+
+        {/* Sign-in form */}
+        <form>
+          <input
+            type="email"
+            placeholder="Email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button onClick={handleSignIn}>Sign In</button>
+        </form>
+
+        {/* Sign-up link */}
+        <p>Dont have an account? <Link to="/sign-up">Sign Up</Link></p>
+
+        {/* Home button */}
+        <p><Link to="/">Back to Home</Link></p>
+      </div>
     </div>
   );
 }
